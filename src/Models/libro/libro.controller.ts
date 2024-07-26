a4e954c05cb0477fd5bd61c42e642457fc76633a
Andres Fariña <farinavaldivia@gmail.com>
2024-07-25 21:15:47 -0400

    Hasta Punto 15 D

--- src/Models/libro/libro.controller.ts
+++ src/Models/libro/libro.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { LibroService } from './libro.service';
+import { Libro } from './libro';
 
 @Controller('libro')
-export class LibroController {}
+export class LibroController {
+
+    constructor(private readonly libroService: LibroService){}
+
+    @Post()
+    crearLibro(@Body() libro: Libro): void {
+        this.libroService.crearLibro(libro);
+    }
+
+    @Get('isbn')
+    obtenerLibro(@Param('isbn') isbn: number): Libro {
+        return this.libroService.buscarLibroPorISBN(isbn);
+    }
+    @Get()
+    obtererLibrosFiltrados(@Param('autor') autor: string, @Param('genero') genero: string): Libro[] {
+        return this.libroService.obtenerLibrosFiltrados(autor, genero);
+    }
+    @Delete(':isbn')
+    eliminarLibro(@Param('isbn') isbn: string): void {
+        this.libroService.eliminarLibro(isbn);
+    }
+
+
+}
+
+
--- src/Models/libro/libro.service.ts
+++ src/Models/libro/libro.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Libro } from './libro';
 
 @Injectable()
-export class LibroService {}
+export class LibroService {
+    buscarLibroPorISBN(isbn: number): Libro {
+        throw new Error('No se encuentra ISBN');
+    }
+    private libros: Libro[] = [];
+
+crearLibro(libro: Libro): void {
+    if (this.libros.some(l => l.isbn == libro.isbn)) {
+        throw new Error('El libro ya está registrado.');
+    }
+    this.libros.push(libro);
+}
+
+obtenerLibros(id: string): Libro {
+    for (let i = 0; i < this.libros.length; i++) {
+        if (this.libros[i].isbn == id) {
+            return this.libros[i];
+        }
+    }
+}
+
+
+obtenerLibrosFiltrados(autor: string, genero: string): Libro[] { 
+    return this.libros.filter(l => l.autor == autor || l.genero == genero);
+}
+
+
+eliminarLibro(ibsn: string): void {
+    for (let i = 0; i < this.libros.length; i++) {
+        if (this.libros[i].isbn == ibsn) {
+            this.libros.splice(i, 1);
+        }
+    }
+}
+
+}
